1f664665fddd55c474310819896ca9e1980cc9ce
Bertrand Thomas <bertrand@devpro.fr>
2023-08-04 17:16:30 +0200

    Make scripts executable

--- scripts/migrate_git_repository.sh
+++ scripts/migrate_git_repository.sh
@@ -1,1 +1,17 @@
 #!/bin/bash
+
+options=(
+  "apple"
+  "milkshake"
+  "salad"
+)
+
+PS3="What would you like for dinner? "
+selected_option=""
+select option in "${options[@]}"
+do
+  selected_option=$option
+  break
+done
+
+echo "Enjoy your $selected_option!"
